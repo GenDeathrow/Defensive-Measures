34bfcfd51a87c04f9c24349c02c6c04e8ddb63b3
vadis365 <curve_linear@hotmail.com>
2016-12-05 22:19:50 +0000

    1st whoops :P - many more to come

--- src/main/java/defensive_measures/init/ModReference.java
+++ src/main/java/defensive_measures/init/ModReference.java
@@ -4,6 +4,6 @@ public class ModReference {
 	public static final String MOD_ID = "defensive";
     public static final String VERSION = "0.0.1";
     public static final String NAME = "Defensive Measures";
-    public static final String PROXY_PATH = "defensive_measures.core.proxies";
+    public static final String PROXY_PATH = "defensive_measures.proxies";
     public static final String CHANNEL_NAME = "vadgendm";
 }
